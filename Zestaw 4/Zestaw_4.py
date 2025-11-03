b6240f7476c4df1323f4129e995c268325fba8e5
theonetrueMax <148761331+theonetrueMax@users.noreply.github.com>
2025-11-03 23:29:59 +0100

    Zestaw 4

--- Zestaw 4/Zestaw_4.py
+++ Zestaw 4/Zestaw_4.py
@@ -95,10 +95,11 @@ def flatten(sequence):
     result = []
     for item in sequence:
         if isinstance(item, (list, tuple)):
-            result.extend(flatten(item))  # rekurencja
+            result.extend(flatten(item)) 
         else:
             result.append(item)
     return result
 
 sequence = [1,(2,3),[],[4,(5,6,7)],8,[9]]
+
 print(flatten(sequence))   # [1,2,3,4,5,6,7,8,9]
