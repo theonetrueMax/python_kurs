531cc4c6cd5bb6a735653440432364f6b46c7115
theonetrueMax <148761331+theonetrueMax@users.noreply.github.com>
2025-12-01 16:18:00 +0100

    6.2

---
+++ Zestaw 6/6.2.py
@@ -0,0 +1,91 @@
+﻿import math
+import unittest
+
+class Point:
+    """Klasa reprezentująca punkty na płaszczyźnie."""
+
+    def __init__(self, x, y): 
+        self.x = x
+        self.y = y
+
+    def __str__(self):    
+        return f"({self.x}, {self.y})"
+
+    def __repr__(self):
+        return f"Point({self.x}, {self.y})"
+
+    def __eq__(self, other):
+        return isinstance(other, Point) and self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+       
+        return self.x * other.x + self.y * other.y
+
+    def cross(self, other):
+        
+        return self.x * other.y - self.y * other.x
+
+    def length(self):
+        return math.hypot(self.x, self.y)     
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+
+class TestPoint(unittest.TestCase):
+
+    def test_str(self):
+        self.assertEqual(str(Point(2, 3)), "(2, 3)")
+        self.assertNotEqual(str(Point(3, 2)), "(2, 3)")
+
+    def test_repr(self):
+        self.assertEqual(repr(Point(2, 3)), "Point(2, 3)")
+        self.assertNotEqual(repr(Point(3, 2)), "(2, 3)")
+
+
+    def test_eq(self):
+        self.assertTrue(Point(1, 2) == Point(1, 2))
+        self.assertFalse(Point(1, 2) == Point(2, 1))
+
+    def test_ne(self):
+        self.assertTrue(Point(1, 2) != Point(2, 3))
+        self.assertFalse(Point(1, 2) != Point(1, 2))
+
+    def test_add(self):
+        self.assertEqual(Point(1, 2) + Point(3, 4), Point(4, 6))
+        self.assertEqual(Point(-1, 2) + Point(-3, -4), Point(-4, -2))
+
+    def test_sub(self):
+        self.assertEqual(Point(5, 5) - Point(2, 3), Point(3, 2))
+        self.assertEqual(Point(5, -5) - Point(-2, -3), Point(7, -2))
+
+    def test_mul(self):
+       
+        self.assertEqual(Point(1, 2) * Point(3, 4), 1*3 + 2*4)
+        self.assertEqual(Point(1, -2) * Point(-3, 4), 1*(-3) + (-2)*4)
+
+    def test_cross(self):
+        
+        self.assertEqual(Point(1, 2).cross(Point(3, 4)), 1*4 - 2*3)
+        self.assertEqual(Point(-1, 2).cross(Point(-3, 4)), -1*4 - 2*(-3))
+
+    def test_length(self):
+        self.assertAlmostEqual(Point(3, 4).length(), 5.0)
+        self.assertAlmostEqual(Point(1, 1).length(), 2**0.5)
+
+    def test_hash(self):
+        s = {Point(1, 2), Point(1, 2)}# set powinien zawierać tylko jeden unikalny punkt jeśli hash działa poprawnie
+        self.assertEqual(len(s), 1)
+
+if __name__ == "__main__":
+    unittest.main()
