ce6645d625287d318a3cf1a2986b32c5917b642f
theonetrueMax <148761331+theonetrueMax@users.noreply.github.com>
2025-12-15 22:21:30 +0100

    10.1

---
+++ Zestaw 10/dice.py
@@ -0,0 +1,24 @@
+﻿import tkinter as tk
+import random
+
+root = tk.Tk()
+root.title("Rzut kostką")
+
+dice_images = [
+   
+    tk.PhotoImage(file=f"assets/{i}.png") for i in range(1, 7)
+]
+
+
+def roll_dice():
+    result = random.randint(0, 5)
+    dice_label.config(image=dice_images[result])
+    dice_label.image = dice_images[result]  
+
+dice_label = tk.Label(root, image=dice_images[0])
+dice_label.pack(pady=20)
+
+roll_button = tk.Button(root, text="Rzuć kostką", command=roll_dice)
+roll_button.pack(pady=10)
+
+root.mainloop()
